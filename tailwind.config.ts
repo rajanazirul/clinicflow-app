33e4b40d28602cb679fc0b33322e4251e96cea44
Raja Nazirul <rajanazirul93@gmail.com>
2024-02-09 09:47:10 +0800

    remove dark theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: ["light", "cupcake"],
   },
 }
 export default config
