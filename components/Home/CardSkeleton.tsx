50a5e96235423920d2bf59dba030a9d1466c78f9
Raja Nazirul <rajanazirul93@gmail.com>
2024-03-07 16:26:07 +0800

    cleanup

---
+++ components/Home/CardSkeleton.tsx
@@ -0,0 +1,15 @@
+export default function CardSkeleton() {
+  return (
+    <div>
+      <div className="rounded-md p-4 max-w-sm w-full mx-auto">
+        <div className="animate-pulse flex space-x-4">
+          <div
+            className="rounded-lg 
+             bg-slate-200 h-[330px] 
+             w-[220px]"
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+}
